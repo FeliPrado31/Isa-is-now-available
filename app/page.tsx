35f8d79600df3807326c24f6de356dd7cad53605
Feli <srgatoiscool@gmail.com>
2024-09-27 20:15:33 -0500

    Refactor code to convert target date to milliseconds in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import CountdownComponent from "./presentation/components/CountdownComponent";
 
 export default function Home() {
-  const targetDate = new Date("2024-10-13T00:00:00").getTime();
+  const targetDate = new Date("2024-10-13T00:00:00").toLocaleString("en-US", {
+    timeZone: "America/Bogota",
+  });
+  const targetDateInMilliseconds = new Date(targetDate).getTime();
 
-  return <CountdownComponent targetDate={targetDate} />;
+  return <CountdownComponent targetDate={targetDateInMilliseconds} />;
 }
